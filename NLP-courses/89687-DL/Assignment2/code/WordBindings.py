a0f9c467500c80a232be48f944a6ab62a7223cf8
kishkash555 <kishkash555@gmail.com>
2018-11-28 12:44:35 +0200

    89687 assingment2 training code works

--- NLP-courses/89687-DL/Assignment2/code/WordBindings.py
+++ NLP-courses/89687-DL/Assignment2/code/WordBindings.py
@@ -6,45 +6,40 @@ import numpy as np
 import pickle
 
 
-## y = softmax(U(tanh(Ex+b))+b')
-def build_ex1_network(nwords, ntags):
+EMB = 50
+INPUT = EMB*5
+HIDDEN = 400
+
+def create_network_params(nwords, ntags):
     # create a parameter collection and add the parameters.
     print("adding parameters")
     m = dy.ParameterCollection()
-    E = m.add_lookup_parameters((nwords,50))
-    b = m.add_parameters((250))
-    U = m.add_parameters((ntags,250))
-    bp = m.add_parameters((ntags))
+    E = m.add_lookup_parameters((nwords,EMB))
+    b = m.add_parameters(HIDDEN)
+    U = m.add_parameters((ntags, HIDDEN))
+    W = m.add_parameters((HIDDEN, INPUT))
+    bp = m.add_parameters(ntags)
     dy.renew_cg()
-    x = dy.vecInput(250)
-    output = dy.softmax( U * (dy.tanh(x + b)) + bp) 
-    return x, m, E, b, U, bp
+    # x = dy.vecInput(250)
+    return m, E, b, U, W, bp
 
-def train_network(x, m, E, b, U, bp, train_data, dev_set):
-    first_x, first_y = next(train_data)
-    print("first x {} first y {}".format(first_x, first_y))
-    x.set(first_x)
-    ntags = bp.npvalue().shape[0]
-    print("*ntags: {}".format(ntags))
-    y = dy.vecInput(ntags)
-    y.set(one_hot(first_y,ntags))
+def build_network(params, x_ordinals):
+    m, E, b, U, W, bp = params
+    x = dy.concatenate([E[ord] for ord in x_ordinals])
+    output = dy.softmax(U * (dy.tanh(W*x + b)) + bp)
+    return output
+
+def train_network(params, ntags, train_data, dev_set):
+    # m, E, b, U, W, bp = params
+    m = params[0]
     
     # train the network
     trainer = dy.SimpleSGDTrainer(m)
     total_loss = 0
     seen_instances = 0
     for train_x, train_y in train_data:
-        # print(train_x, train_y)
         dy.renew_cg()
-        a = train_x
-        e0 = E[a[0]]
-        e1 = E[a[1]]
-        e2 = E[a[2]]
-        e3 = E[a[3]]
-        e4 = E[a[4]]
-        e05 = dy.concatenate([e0, e1, e2, e3, e4])
-        output = dy.softmax( U * (dy.tanh(e05 + b)) + bp)
-        y.set(one_hot(train_y,ntags))
+        output = build_network(params, train_x)
         loss = -dy.log(output[train_y])
 
         seen_instances += 1
@@ -52,38 +47,23 @@ def train_network(x, m, E, b, U, bp, train_data, dev_set):
         loss.backward()
         trainer.update()
         
-        if seen_instances % 1000 == 0:
+        if seen_instances % 20000 == 0:
             print("average loss after {} iterations: {}. current loss: {}".format(seen_instances, total_loss / seen_instances, loss.value()))
-            with open("ex1_embed_mat.pickle",'wb') as f:
-                pickle.dump(E.value(),f)
-            #print("E is: {}".format(E.value()))
             good = case = 0
-            for x_tuple, dev_y in list(generate_train_tuples(dev_set,word_dict, tag_dict))[:50]:
-                dy.renew_cg()
-                e = [E[x] for x in x_tuple]
-                e5 = dy.concatenate(e)
-                output = dy.softmax( U * (dy.tanh(e5 + b)) + bp)
-                #print(x_tuple, output.npvalue(), dev_y)
+            max_dev_instances = 50*1000
+            dev_instances = 0
+            for x_tuple, dev_y in dev_set:
+                #dy.renew_cg()
+                output =  build_network(params, x_tuple)
                 if np.argmax(output.npvalue()) == dev_y:
                     good +=1
                 case +=1
+                dev_instances += 1
+                if dev_instances >= max_dev_instances:
+                    break
             print("accuracy after {} iterations: {}".format(seen_instances, float(good)/case))
     return 
 
-
-# function for graph creation
-# def create_network_return_loss(inputs, expected_output):
-#     """
-#     inputs is a list of numbers
-#     """
-#     dy.renew_cg()
-#     emb_vectors = [lookup[i] for i in inputs]
-#     net_input = dy.concatenate(emb_vectors)
-#     net_output = dy.softmax( (pW*net_input) + pB)
-#     loss = -dy.log(dy.pick(net_output, expected_output))
-#     return loss
-
-#     loss = dy.binary_log_loss()
 
 def scan_train_for_vocab(train_data):
     words = Counter()
@@ -100,7 +80,7 @@ def scan_train_for_vocab(train_data):
     tag_dict = OrderedDict((a,i) for i, a in enumerate(tag_list))
     return word_dict, tag_dict
 
-def generate_tagged_sentences(input_file):
+def train_stream_to_sentence_tuples(input_file):
     sentence = [('**START**', '')]*2
     for line in input_file:
         if len(line)>1:
@@ -127,31 +107,31 @@ def generate_train_data(tagged_sentences, word_dict, tag_dict):
             yield word_oh, tag_oh
 
 
-def generate_train_tuples(tagged_sentences, word_dict, tag_dict):
+def generate_train_5tuples(tagged_sentence_stream, word_dict, tag_dict, unk_threshold):
     """
     generate a 5-tuple of indices
     and a y one-hot vector
     based on the current word + 2 words of context from each side
     """
-    for tagged_sentence in tagged_sentences:
+    for tagged_sentence in tagged_sentence_stream:
         train_x_tuple = []
         train_y_tuple = []
         for word, tag in tagged_sentence:
             if word in word_dict:
                 train_x_tuple.append(word_dict[word])
-            #else: 
-            #    train_x_tuple.append(word_dict['**UNK**'])
+            else: 
+                train_x_tuple.append(word_dict['**UNK**'])
             train_y_tuple.append(tag)
             if len(train_x_tuple) == 5:
-                yield train_x_tuple, tag_dict[train_y_tuple[2]]
-                # print("tagged sentence: {}.\ntrain_x:{}\ttag:{}".format(tagged_sentence, train_x_tuple, tag_dict[train_y_tuple[2]]))
-                train_x_tuple = train_x_tuple[1:]
-                train_y_tuple = train_y_tuple[1:]
+                yield list(train_x_tuple), tag_dict[train_y_tuple[2]]
+                
+                train_x_tuple.pop(0)
+                train_y_tuple.pop(0)
                 
 
 
 
-def tagged_sentence_to_train_data(tagged_sentence,word_dict, tag_dict):
+def tagged_sentence_to_train_data(tagged_sentence, word_dict, tag_dict):
     """
     for now, does not implement embeddings
     """
@@ -175,31 +155,21 @@ if __name__ == "__main__":
     word_dict['**UNK**'] = len(word_dict)
     tag_dict[''] = len(tag_dict)
     
-    x, m, E, b, u, bp = build_ex1_network(len(word_dict), len(tag_dict))
+
+    params = create_network_params(len(word_dict), len(tag_dict))
 
     with open(dev_file, 'rt') as d:
-        dev_set = list(generate_tagged_sentences(d))
+        dev_set = list(generate_train_5tuples(train_stream_to_sentence_tuples(d), word_dict, tag_dict, 0))
 
-    dev_set = dev_set[:len(dev_set)/10]
 
-    #print(list(generate_train_tuples(dev_set[:10],word_dict, tag_dict)))
-    print(tag_dict)
+    # print(list(generate_train_tuples(dev_set[:10],word_dict, tag_dict)))
+    # print(tag_dict)
+    unk_threshold = int(len(word_dict)/10) + 1
+    ntags = len(tag_dict)
     with open(input_file,'rt') as i:
-        train_data_iter = generate_train_tuples(generate_tagged_sentences(i), word_dict, tag_dict)
+        train_data = list(generate_train_5tuples(train_stream_to_sentence_tuples(i), word_dict, tag_dict, unk_threshold))
 
-        # for j in range(5):
-        #    train_data_iter.next()
-        train_network(x, m, E, b, u, bp, train_data_iter, dev_set)
+    print(train_data[:5])
+    train_network(params, ntags, train_data, dev_set)
   
-  
-        # for tagged_word in sentence:
-        #         train_network
-        #     tagged_sentence_to_train_data
-        #     codes_sentence = [(word_dict[a],tag_dict[b]) for a,b in sentence]
-        #     print(sentence)
-        #     print(codes_sentence)
-            
-        #     c += 1
-        #     if c==5: 
-        #         break
             

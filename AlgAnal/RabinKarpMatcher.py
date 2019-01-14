2401c7109b9931c051329cae144076f1c04bcb9e
kishkash555 <kishkash555@gmail.com>
2019-01-14 14:08:08 +0200

    AlgAnal: Rabin Karp Matcher work on code

--- AlgAnal/RabinKarpMatcher.py
+++ AlgAnal/RabinKarpMatcher.py
@@ -1,7 +1,7 @@
 class rk_hash:
     def __init__(self, d,q):
-        self.d = d
-        self.q = q
+        self.d = d # the polynom basis 
+        self.q = q # the prime number defining the field
         #self.H = pow(d,m-1) % q
         self.current_hash = 0
         self.current_string = []
@@ -59,24 +59,24 @@ def rk_patter_match(text, pattern, d, q):
 def Rabin_Karp_Matcher(text, pattern, d, q):
     n = len(text)
     m = len(pattern)
-    h = pow(d,m-1)%q
+    h = pow(d,m-1)%q # the shift of the mth character
     p = 0
     t = 0
     result = []
     for i in range(m): # preprocessing
         p = (d*p+ord(pattern[i]))%q
         t = (d*t+ord(text[i]))%q
-    for s in range(n-m+1): # note the +1
+    for s in range(n-m+1): # note the +1: ensures last position is tested
         if p == t: # check character by character
             match = True
-            for i in range(m):
+            for i in range(m): # "cheat": if probablitstic algorithm finds a match, we verify it deterministically
                 if pattern[i] != text[s+i]:
                     match = False
                     break
             if match:
                 result = result + [s]
         if s < n-m:
-            t = (t-h*ord(text[s]))%q # remove letter s
+            t = (t-h*ord(text[s]))%q # remove s'th letter of text
             t = (t*d+ord(text[s+m]))%q # add letter s+m
             t = (t+q)%q # make sure that t >= 0
     return result

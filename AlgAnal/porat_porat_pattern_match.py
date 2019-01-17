d54bf7dff9a96f33c078526d3a04e769018f7c4f
kishkash555 <kishkash555@gmail.com>
2019-01-17 17:44:50 +0200

    AlgAnal porat port

---
+++ AlgAnal/porat_porat_pattern_match.py
@@ -0,0 +1,9 @@
+class process():
+    def __init__(self, signatures):
+        self.text_signature = []
+        self.signatures = signatures
+        self.next_power_of_2=1
+    def update(char):
+        self.text_signature.append(char)
+        if len(self.text_signature) ==  self.next_power_of_2:
+            if 

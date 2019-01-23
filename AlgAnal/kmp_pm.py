d6732496e9647419df64ea123ac0d62725b8da41
kishkash555 <kishkash555@gmail.com>
2019-01-23 17:05:52 +0200

    after a few tweaks

--- AlgAnal/kmp_pm.py
+++ AlgAnal/kmp_pm.py
@@ -1,5 +1,5 @@
 class KMP:
-    def partial(self, pattern):
+    def __init__(self, pattern):
         """ Calculate partial match table: String -> [Int]"""
         ret = [0]
         
@@ -9,20 +9,28 @@ class KMP:
             pj = pattern[j]
             while j > 0 and pi != pj:
                 j = ret[j - 1]
+                pj = pattern[j]
             ret.append(j + 1 if pattern[j] == pattern[i] else j)
-        return ret
+        self.partial = ret
+        self.P = pattern
         
-    def search(self, T, P):
+    def search(self, T):
         """ 
         KMP search main algorithm: String -> String -> [Int] 
         Return all the matching position of pattern string P in S
         """
-        partial, ret, j = self.partial(P), [], 0
+        P, partial, ret, j = self.P, self.partial, [], 0
         
         for i in range(len(T)):
+            ti = T[i]
+            pj = P[j]
+            matched = T[i-j : i+1]
             while j > 0 and T[i] != P[j]:
                 j = partial[j - 1]
-            if T[i] == P[j]: j += 1
+                pj = P[j]
+            matched = T[i-j : i+1]
+            if ti == pj: j += 1
+            # matched = T[max(0,i-j) : i+1]
             if j == len(P): 
                 ret.append(i - (j - 1))
                 j = partial[j - 1]
@@ -31,4 +39,5 @@ class KMP:
 
 
 if __name__ == "__main__":
-    kmp = KMP().partial('aaaabbbbcccdddaaa')
+    kmp = KMP('ababababcccddd')
+    kmp.search('abababababababcccdddaaaabbbccdddaaaaa')

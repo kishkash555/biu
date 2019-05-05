57a98177cb9c6b20e77fbd150cc2016d55f6a817
kishkash555 <kishkash555@gmail.com>
2019-05-05 17:07:34 +0300

    speech ex2 without norm 45% accuracy

--- 2019-2/SpeechRecog/ex2_dtw/speech_ex2.py
+++ 2019-2/SpeechRecog/ex2_dtw/speech_ex2.py
@@ -3,12 +3,17 @@ import librosa
 from os import path
 import glob
 from collections import defaultdict
+import argparse
+
 TEST_DIR = "test_files"
 TRAIN_DIR = "train_files"
 FILES_GLOB = "*.wav"
 
 N_FEATURES = 20
-
+LABELS_TO_NUMBERS = {
+    "one": 1, "two": 2, "three": 3, "four": 4, "five": 5
+    }
+    
 euclidean_dist = lambda v1, v2: np.linalg.norm(v2-v1)
 
 class naive_feature_extractor:
@@ -22,9 +27,9 @@ extract = naive_feature_extractor.extract
 def load(fname):
     return librosa.load(fname, sr=None)
 
-def main():
+def main(is_normalize_inputs=False, is_normalize_features=False):
     train_files = list(glob.glob(path.join(TRAIN_DIR,'*',FILES_GLOB)))
-    test_files = list(glob.glob(path.join(TEST_DIR,FILES_GLOB)))
+    test_files = sorted(glob.glob(path.join(TEST_DIR,FILES_GLOB)))
     
     train_x = []
     train_y = []
@@ -32,9 +37,13 @@ def main():
     for trn in train_files:
         label = path.split(path.split(trn)[0])[1]
         x_trn, sr_train = load(trn)
+        if is_normalize_inputs:
+            x_trn = normalize_vec(x_trn)
         feature_mat = extract(x_trn,sr_train)
+        if is_normalize_features:
+            feature_mat = normalize_columns(feature_mat)
         train_x.append(feature_mat.T)
-        train_y.append(label)
+        train_y.append(LABELS_TO_NUMBERS[label])
         
     clsfier1 = nn1(lambda v1, v2: dynamic_time_warping(v1, v2, euclidean_dist)).fit(train_x, train_y)
     clsfier2 = nn1(euclidean_dist).fit(train_x, train_y)
@@ -42,12 +51,24 @@ def main():
     for tst in test_files:
         tst_fname = path.basename(tst)
         x_test, sr_test = load(tst)
+        
+        if is_normalize_inputs:
+            x_test = normalize_vec(x_test)
+        
         tst_features = extract(x_test, sr_test)
+        if is_normalize_features:
+            tst_features = normalize_columns(tst_features)
+
         cls1 = clsfier1.predict(tst_features.T)
         cls2 = clsfier2.predict(tst_features.T)
-        print("{} - {} - {}".format(tst_fname, cls1, cls2))
+        fprint("{} - {} - {}".format(tst_fname, cls2, cls1))
 
 
+def normalize_vec(v):
+    return (v-v.mean()) / v.std()
+
+def normalize_columns(m):
+    return (m-np.average(m,axis=0))/np.std(m,axis=0)
 
 class nn1:
     def __init__(self, distance_metric):
@@ -104,8 +125,39 @@ def test_dtw():
     ret = dynamic_time_warping(vec1, vec2, dist)
     assert(ret==15)
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('output_file', action='store', nargs='?')
+    args = parser.parse_args()
+    return args
+
+class manage_fprint:
+    def __init__(self, args):
+        self.f = None
+        if args.output_file:
+            self.f = open(args.output_file,'wt',buffering=1)
+
+    def get_fprint(self):
+        def fprint(s):
+            print(s)
+            print(s, file=self.f)
+        if self.f:
+            return fprint
+        return print
+
+    def close(self):
+        self.f.close()
+
+
 if __name__ == "__main__":
-    main()
+    global fprint
+    args = parse_args()
+    mfp = manage_fprint(args)
+    fprint = mfp.get_fprint()
+    
+    main(is_normalize_inputs=False, is_normalize_features=False)
+    mfp.close()
+
 
     # y, sr = librosa.load(f_path, sr=None)
     # mfcc = librosa.feature.mfcc(y=y, sr=sr)

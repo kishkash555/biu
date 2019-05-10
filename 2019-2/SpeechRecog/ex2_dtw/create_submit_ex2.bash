cfe15e076ff9f54729c671b9464724135a952ad7
kishkash555 <kishkash555@gmail.com>
2019-05-10 11:13:06 +0300

    script for SpeehRecog ex2, start work on MachLearn ex2

--- 2019-2/SpeechRecog/ex2_dtw/create_submit_ex2.bash
+++ 2019-2/SpeechRecog/ex2_dtw/create_submit_ex2.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
-mkdir -p ex2_submit
+rm -rf ex2_submit
+mkdir ex2_submit
 cp speech_ex2.py ex2_submit/ex2.py
 echo "shahar siegman" > ex2_submit/details.txt
 echo "011862141" >> ex2_submit/details.txt
-cp ex2_24da3d_out.txt ex2_submit/output.txt
+cp ex2_0426cf_out.txt ex2_submit/output.txt
 zip ex2_shahar.zip ex2_submit/*
 

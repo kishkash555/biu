8d73792a97b703aba77840fcebd7edb74e75290f
kishkash555 <kishkash555@gmail.com>
2019-06-04 13:26:23 +0300

    added train and stepsize management functionality

---
+++ 2019-2/MachLearn/ex3/data_iterator.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class data_iterator:
+    def __init__(self, array_x, array_y, batch_size, shuffle = True):
+        self.array_x = array_x
+        self.array_y = array_y.squeeze()
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.length = array_x.shape[0]
+
+    def __iter__(self):
+        self.schedule = np.arange(self.length, dtype=int)
+        if self.shuffle:
+            np.random.shuffle(self.schedule)
+        self.n = 0
+        return self
+    
+    def __next__(self):
+        if self.n == self.length:
+            return StopIteration
+        n, next_n = self.n, min(self.batch_size, self.length)
+        self.n = next_n
+        return self.array_x[self.schedule[n:next_n],:], self.array_y[self.schedule[n:next_n]]
--- 2019-2/MachLearn/ex3/gazir.py
+++ 2019-2/MachLearn/ex3/gazir.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.special import softmax
-from collections import OrderedDict
+from collections import OrderedDict, namedtuple
+
+train_options_nt = namedtuple('train_options',['epochs','report_interval'])
 
 class layer:
     def __init__(self):
@@ -12,7 +14,7 @@ class layer:
     def backward(self, grad_output):
         raise NotImplementedError()
     
-    def loss(self, y):
+    def grad_loss(self, y):
         raise NotImplementedError()
 
     def update(self, stepsize):
@@ -29,15 +31,15 @@ class linear_layer(layer):
     
     def forward(self, x):
         W, b = self.parameters.values()
-        self.ctx['input'] = x
+        self.ctx['input'] = x.copy()
         return x.dot(W) + b
 
     def backward(self, grad_output):
         W, _ = self.parameters.values()
         x = self.ctx['input']
-        grad_input = grad_output.dot(W)
-        grad_weight = grad_output.T.dot(x)
-        grad_bias = grad_output
+        grad_input = grad_output.dot(W.T)
+        grad_weight = x.T.dot(grad_output)
+        grad_bias = np.sum(grad_output,axis=0)
         self.ctx['grads'] = grad_weight, grad_bias
         return grad_input
 
@@ -66,19 +68,20 @@ class relu_layer(layer):
 
 class softmax_nll_layer(layer):
     def __init__(self):
-        super().__init__(self)
+        super().__init__()
     
     def forward(self, x):
-        ret = softmax(x)
+        ret = softmax(x, axis=1)
         self.ctx['softmax'] = ret.copy()
         return ret
 
     def backward(self, grad_output): 
         pass
     
-    def loss(self, y):
-        ret = -self.ctx['softmax']
-        ret[y] = ret[y] + 1
+    def grad_loss(self, y):
+        ret = self.ctx['softmax']
+        ret[:,y] = ret[:,y] - 1
+        # ret = np.sum(ret,axis=0)
         return ret
 
 
@@ -88,24 +91,47 @@ class network(layer):
         super().__init__()
         layer_names = layer_names or ['layer{:02}'.format(i) for i in range(len(layer_obj))]
         self.layers = OrderedDict(zip(layer_names, layer_obj))
+        self.train_options = train_options_nt(20, 2000)
     
+    def set_train_options(self, **kwargs):
+        self.train_options = self.train_options._replace(**kwargs)
+
     def forward(self, x):
         z = x
-        for layer_name, layer_obj in self.layers.items():
+        for layer_obj in self.layers.values():
             z = layer_obj.forward(z)
         return z
 
     def backward(self, y):
         is_last_layer = True
-        for layer_name, layer_obj in reversed(self.layers.items()):
+        for layer_obj in reversed(self.layers.values()):
             if is_last_layer:
-                grad = layer_obj.loss(y)
+                grad = layer_obj.grad_loss(y)
                 is_last_layer = False
             else:
                 grad = layer_obj.backward(grad)
-
+        return grad
+        
     def update(self, stepsize):
-        for layer_name, layer_obj in self.layers.items():
+        for layer_obj in self.layers.values():
             layer_obj.update(stepsize)
 
+    def train(self, train_set, lr, validation_set = None):
+        """
+        train_set: an iterable of x,y pairs, possibly batched
+        lr: learning rate object 
+        validation_set: an iterable of x,y pairs (possibly batched) for checking the accuracy
+        """
+        lr_gen = lr.lr_generator
+        to = self.train_options
+        for ep in range(to.epochs):
+            for i, (x, y) in enumerate(iter(train_set)):
+                st = next(lr_gen)
+                self.forward(x)
+                self.backward(y)
+                self.update(st)
+                if i % to.report_interval == 0:
+                    # report code here
+                    pass 
+            lr.new_epoch()
 

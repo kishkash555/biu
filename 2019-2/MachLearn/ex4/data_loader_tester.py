ad594a621a847f629044122a84313a37f8a05d4f
kishkash555 <kishkash555@gmail.com>
2019-06-29 19:30:36 +0300

    after some refactor

--- 2019-2/MachLearn/ex4/data_loader_tester.py
+++ 2019-2/MachLearn/ex4/data_loader_tester.py
@@ -4,8 +4,13 @@ import torch
 dataset = GCommandLoader('./data/train')
 
 test_loader = torch.utils.data.DataLoader(
-        dataset, batch_size=100, shuffle=None,
+        dataset, batch_size=100, shuffle=True,
         num_workers=0, pin_memory=True, sampler=None)
+
+test_loader = list(test_loader)
 
 for k, (input,label) in enumerate(test_loader):
     print(input.size(), len(label))
+    if k == 100:
+            break
+
--- 2019-2/MachLearn/ex4/ex4.py
+++ 2019-2/MachLearn/ex4/ex4.py
@@ -15,58 +15,98 @@ IN_CHANNELS = 161
 SIGNAL_LENGTH = 101
 N_CLASSES = 31
 
+def n_out(input_size, padding, kernel_size, stride, dilation):
+    return int((input_size + 2*padding - dilation*(kernel_size - 1) -1)/stride +1 )
 
-class cv1:
-    input_size = (SIGNAL_LENGTH, IN_CHANNELS)
+def conv_output_size(conv):
+    return (
+        conv.out_channels if hasattr(conv,'out_channels') else conv.input_size[0], 
+        n_out(conv.input_size[1], conv.padding, conv.kernel_size, conv.stride, conv.dilation),
+        n_out(conv.input_size[2], conv.padding, conv.kernel_size, conv.stride, conv.dilation)
+        )
+
+def mult(s):
+    return s[0]*s[1]*s[2]
+
+class conv_default:
+    dilation = 1
+    padding = 0
+    stride = 1    
+
+class pl_default:
+    dilation = 1
+    padding = 0
+
+class cv1(conv_default):
+    input_size = (1, SIGNAL_LENGTH, IN_CHANNELS)
     in_channels = 1
     out_channels = 32
     kernel_size = 3
     stride = 2
-    output_size = ( out_channels, 
-        int((input_size[0] - kernel_size + 1)/stride),
-        int((input_size[1] - kernel_size + 1)/stride)
-        )
+    padding = 1
+cv1.output_size = conv_output_size(cv1)
 
 print("cv1 output: {}".format(cv1.output_size))
 
-class pl1:
+class pl1(pl_default):
     input_size = cv1.output_size
-    kernel_size = 3
+    kernel_size = 2
     stride = kernel_size
-    output_size = ( 
-        input_size[0], 
-        int((input_size[1] - kernel_size)/stride + 1),
-        int((input_size[2] - kernel_size)/stride + 1),
-    )
+pl1.output_size = conv_output_size(pl1)
 
-class cv2:
-    input_size = (cv1.output_size[1], cv1.output_size[2])
+print("pl1 output: {}".format(pl1.output_size))
+
+class cv2(conv_default):
+    input_size = pl1.output_size
     in_channels = pl1.output_size[0]
+    out_channels = 16
+    kernel_size = 3
+    stride = 3
+    output_size = (out_channels,
+        int((input_size[1] - kernel_size)/stride + 1),
+        int((input_size[2] - kernel_size)/stride + 1))
+
+print("cv2 output: {} ({})".format(cv2.output_size, mult(cv2.output_size)))
+
+
+class pl2(pl_default):
+    input_size = cv2.output_size
+    kernel_size = 2
+    stride = kernel_size
+pl2.output_size = conv_output_size(pl2)
+
+print("pl2 output: {} ({})".format(pl2.output_size, mult(pl2.output_size)))
+
+
+
+class cv3:
+    input_size = pl2.output_size
+    in_channels = pl2.output_size[0]
     out_channels = 32
     kernel_size = 3
-    stride = 2
+    stride = 1
     output_size = (out_channels,
-        int((input_size[1] - kernel_size + 1)/stride),
-         int((input_size[0] - kernel_size + 1)/stride))
+        int((input_size[1] - kernel_size)/stride + 1),
+        int((input_size[2] - kernel_size)/stride + 1))
 
-print("cv2 output: {}".format(cv2.output_size))
+print("cv3 output: {}".format(cv3.output_size))
 
 
-class pl2:
-    input_size = cv2.output_size
+class pl3:
+    input_size = cv3.output_size
     kernel_size = 3
     stride = kernel_size
     output_size = ( 
         input_size[0], 
-        int((input_size[1] - kernel_size)/stride + 1),
+        int((input_size[1] - kernel_size +1 )/stride - 1),
         int((input_size[2] - kernel_size)/stride + 1),
     )
 
-print("pl2 output: {}".format(pl2.output_size))
+print("pl3 output: {}".format(pl3.output_size))
 
 
 class fc1:
-    input_size = pl2.output_size[0]*pl2.output_size[1]*pl2.output_size[2]
+    input_size = mult(pl2.output_size) 
     output_size = 100
 
 print("fc1 input {}".format(fc1.input_size))
@@ -80,11 +120,14 @@ class convnet(nn.Module):
     def __init__(self, min_acc=0.75, epochs=20, logging_interval=150, save_fname='model_file'):
         super().__init__()
         # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros')
-        self.conv1 = nn.Conv2d(cv1.in_channels, cv1.out_channels, cv1.kernel_size)
+        self.conv1 = nn.Conv2d(cv1.in_channels, cv1.out_channels, cv1.kernel_size, cv1.stride, cv1.padding)
         self.pool1 = nn.MaxPool2d(pl1.kernel_size)
         
-        self.conv2 = nn.Conv2d(cv2.in_channels, cv2.out_channels, cv2.kernel_size)
+        self.conv2 = nn.Conv2d(cv2.in_channels, cv2.out_channels, cv2.kernel_size, cv2.stride, cv2.padding)
         self.pool2 = nn.MaxPool2d(pl2.kernel_size)
+        
+        #self.conv3 = nn.Conv2d(cv3.in_channels, cv3.out_channels, cv3.kernel_size)
+        #self.pool3 = nn.MaxPool2d(pl3.kernel_size)
         
         self.fc1 = nn.Linear(fc1.input_size,fc1.output_size)
         self.fc2 = nn.Linear(fc2.input_size,fc2.output_size)
@@ -100,16 +143,15 @@ class convnet(nn.Module):
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = self.pool1(x)
-        print("after pool1 {}".format(x.shape))
+        # print("after pool1 {}".format(x.shape))
 
         x = F.relu(self.conv2(x))
         x = self.pool2(x)
-        print("after pool2 {}".format(x.shape))
+        # print("after pool2 {}".format(x.shape))
         
-
         x = x.view(-1,fc1.input_size)
 
-        print("after flat {}".format(x.shape))
+        # print("after flat {}".format(x.shape))
 
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
@@ -182,12 +224,13 @@ def main():
     valid_set = GCommandLoader('./data/valid')
     
     train_loader = torch.utils.data.DataLoader(
-            train_set, batch_size=100, shuffle=False,
+            train_set, batch_size=100, shuffle=True,
             num_workers=30, pin_memory=False, sampler=None)
     
+    train_loader = list(train_loader)
     valid_loader = torch.utils.data.DataLoader(
             valid_set, batch_size=100, shuffle=None,
-            num_workers=5, pin_memory=True, 
+            num_workers=5, pin_memory=False, 
             sampler=None )
     
     net = convnet()

eb08c77e8d833fd3199b48c6e539c011e9de4b0f
kishaksh <kiskash555@gmail.com>
2020-03-28 22:59:30 +0300

    after moving decimation function

--- 2020-1/DSP2/matlab_ex1/pp_decimate.m
+++ 2020-1/DSP2/matlab_ex1/pp_decimate.m
@@ -1,16 +1,24 @@
-function y=pp_decimate(x,h,M)
+function y=pp_decimate2(x,h,M)
 h = pad_divisible(h,M);
 x = pad_divisible(x,M);
 
 h_pp = reshape(h, M, length(h)/M);
-x_pp = reshape(x, M, length(x)/M);
+
+t = reshape(-M+1:length(x),M,length(x)/M+1);
+t = t(M:-1:1,:);
+ind_t = t+1;
+ind_t(ind_t<1)=1;
+ind_t(ind_t>length(x))=1;
+x_pp = x(ind_t);
+x_pp(t<0)=0;
+
 
 y = zeros(1,size(h_pp,2)+size(x_pp,2)-1);
 
 for i=1:M
-    new_comp = conv(h_pp(i,:),x_pp(i,:));
-    new_comp = circshift(new_comp,i);
+    curr_x = x_pp(i,:);
+    new_comp = conv(h_pp(i,:),curr_x);
     y = y + new_comp;
 end
-    y=circshift(y,-1);
+
 end

92619f5a2e63016a1f6cd0a09f1d4719d61655dc
kishaksh <kiskash555@gmail.com>
2020-06-28 13:46:33 +0300

    cohesion classifier

--- Dissertation3/code/cohesion_classifier.py
+++ Dissertation3/code/cohesion_classifier.py
@@ -6,29 +6,44 @@ import numpy as np
 import loader
 
 class ann_model(nn.Module):
-    #conv1d(in_channels: int, out_channels: int, kernel_size, stride: ..., padding: ..., dilation: ..., groups: int, bias: bool, padding_mode: str) -> None
+  #conv1d(in_channels: int, out_channels: int, kernel_size, stride: ..., padding: ..., dilation: ..., groups: int, bias: bool, padding_mode: str) -> None
+  #MaxPool1d(kernel_size, stride=None, padding=0, dilation=1,
   def __init__(self):
     super(ann_model,self).__init__()
-    self.conv1 = nn.Conv1d(1, 8, 4, 1, 5)
-    self.pool1 = nn.MaxPool1d(4)
-    self.conv2 = nn.Conv1d(8, 4, 4, 1, 5)
-    self.pool2 = nn.MaxPool1d(4)
-    self.conv3 = nn.Conv1d(4, 1, 4, 1, 0)
-    self.pool3 = nn.MaxPool1d(4)
-    self.linear1 = nn.Linear(6,1)
+    self.conv1 = nn.Conv1d(1, 64, 21, 5, 5)
+    self.conv2 = nn.Conv1d(64, 64, 21, 1, 5)
+    self.pool1 = nn.MaxPool1d(2,2)
 
+    self.conv3 = nn.Conv1d(64, 128, 5, 1, 0)
+    self.conv4 = nn.Conv1d(128, 128, 5, 1, 0)
+    self.pool2 = nn.MaxPool1d(2,2)
+
+    self.conv5 = nn.Conv1d(128, 256, 6, 1, 0)
+    self.conv6 = nn.Conv1d(256, 256, 6, 1, 0)
+    self.pool3 = nn.AvgPool1d(3)
+    self.linear1 = nn.Linear(256,1)
+#    self.linear2 = nn.Linear(6,1)
  
   def forward(self,x):
     x = self.conv1(x)
     x = F.relu(x)
-    x = self.pool1(x)
     x = self.conv2(x)
     x = F.relu(x)
-    x = self.pool2(x)
+    x = self.pool1(x)
+
     x = self.conv3(x)
     x = F.relu(x)
-    x = self.pool3(x)
+    x = self.conv4(x)
+    x = F.relu(x)
+    x = self.pool2(x)
+    x = self.conv5(x)
+    x = F.relu(x)
+    x = self.conv6(x)
+    x = F.relu(x)
+    x = self.pool3(x).view(-1)
     x = self.linear1(x)
+#    x = F.relu(x)
+#    x = self.linear2(x)
     x = torch.sigmoid(x)
     return x
 
@@ -44,7 +59,7 @@ def train(model, epochs):
     for id, x, y in loader.loader(None,'disk'):
       optimizer.zero_grad()
       y_hat = model(x)
-      loss = criterion(y_hat,torch.Tensor([[[y]]]))
+      loss = criterion(y_hat.view(1),torch.Tensor([y]))
 
       loss.backward()
 
@@ -56,7 +71,7 @@ def train(model, epochs):
       epochloss += loss.item()
       l += 1
     losslist.append(epochloss/l)
-    print("======> epoch: {}/{}, Loss:{}, l: {}\n{}".format(epoch,epochs,epochloss, l, model.linear1.weight))
+    print("======> epoch: {}/{}, Loss:{}, l: {}\n".format(epoch,epochs,epochloss, l))
     epochloss=0
 
 

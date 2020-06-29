a9fc533a07e5aada9b30ad0b7d54bd4e5cc9ac25
kishaksh <kiskash555@gmail.com>
2020-06-29 10:15:25 +0300

    split to train and validation; writing the encoder-decoder architecture add split toggle to loader; autoencoder with exploding gradient problem normalized each vector separately to zero mean changed train/validation split to 0.8/0.2

--- Dissertation3/code/loader.py
+++ Dissertation3/code/loader.py
@@ -4,6 +4,7 @@ import pandas as pd
 import glob
 from os import path, sep
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 BASE_LEN = 400
 
@@ -34,36 +35,62 @@ def get_data_from_sql(con):
     cohesion_data.to_pickle('cohesion_data.pkl')
     return all_data, cohesion_data
 
-def loader(con, source='disk'):
-    if source == 'disk':
-        all_data = pd.read_pickle('all_data.pkl')
-        cohesion_data = pd.read_pickle('cohesion_data.pkl')
-    else: 
-        all_data, cohesion_data = get_data_from_sql(con)
-    all_data = all_data.set_index("signal_id")
-    signal_id_list = list( all_data.index.unique())
-    np.random.shuffle(signal_id_list)
+class loader:
+    def __init__(self, con, source='disk', split=False):
+        if source == 'disk':
+            all_data = pd.read_pickle('all_data.pkl')
+            cohesion_data = pd.read_pickle('cohesion_data.pkl')
+        else: 
+            all_data, cohesion_data = get_data_from_sql(con)
+        
+        all_data["pars"] = all_data.signal_id.map(lambda x: x.split("_")[0])
+        pars = all_data.pars.unique()
+        if split:
+            train_pars, validation_pars = train_test_split(pars, test_size=0.2,shuffle=False)
+        else:
+            train_pars = pars
+            validation_pars = []
+            
+        all_data["learn_group"]=all_data.pars.map(
+            lambda x: x in set(train_pars) and "train" or "validation"
+            )
+            
+        #self.all_data = all_data
+        self.cohesion_dict = { 
+            str(int(par)): co 
+            for _, (par, co) in cohesion_data.iterrows()
+            }
 
-    cohesion_dict = {str(int(par)): co for _, (par, co) in cohesion_data.iterrows()}
-    
-    for sig_id in signal_id_list:
-        data = all_data.loc[sig_id].reset_index()
-        par = sig_id.split("_")[0]
-        coh = cohesion_dict.get(par)
-        if not coh:
-            #print("not found for {}".format(par))
-            continue
-        if np.isnan(coh):
-            #print("nan found for {}".format(par))
-            continue
+        self.all_data = all_data.set_index("signal_id")
+     
+    def generator(self,training=True):
+        learn_group = training and "train" or "validation"
+        curr_df = self.all_data[self.all_data.learn_group==learn_group]
+        
+        signal_id_list = list(curr_df.index.unique())
+        np.random.shuffle(signal_id_list)
 
-        min_ord = data.data_ordinal.iloc[0]
-        max_ord = data.data_ordinal.iloc[-1]
-        data = data.set_index("data_ordinal")
-        for i in range(min_ord, max_ord-BASE_LEN,BASE_LEN):
-            x = torch.FloatTensor(data.loc[i:i+BASE_LEN-1,"data_value"].values - 0.8) * 10
-            yield sig_id, x.reshape(1,1,-1), coh
+        for sig_id in signal_id_list:
+            data = self.all_data.loc[sig_id].reset_index()
+            par = sig_id.split("_")[0]
+            coh = self.cohesion_dict.get(par)
+            if not coh:
+                #print("not found for {}".format(par))
+                continue
+            if np.isnan(coh):
+                #print("nan found for {}".format(par))
+                continue
 
+            min_ord = data.data_ordinal.iloc[0]
+            max_ord = data.data_ordinal.iloc[-1]
+            data = data.set_index("data_ordinal")
+            for i in range(min_ord, max_ord-BASE_LEN,BASE_LEN):
+                x = data.loc[i:i+BASE_LEN-1,"data_value"].values
+                x = torch.FloatTensor(x- x.mean())
+                yield sig_id, x.reshape(1,1,-1), coh
 
+class abcd:
+    def __init__(self):
+        super().__init__()
 def connect():
     return engine.connect()
--- Dissertation3/code/sparse_ae.py
+++ Dissertation3/code/sparse_ae.py
@@ -8,61 +8,58 @@ import loader
 class ann_model(nn.Module):
   #conv1d(in_channels: int, out_channels: int, kernel_size, stride: ..., padding: ..., dilation: ..., groups: int, bias: bool, padding_mode: str) -> None
   #MaxPool1d(kernel_size, stride=None, padding=0, dilation=1,
+
+  # torch.nn.ConvTranspose1d(in_channels: int, out_channels: int, kernel_size, 
+  # stride, padding, output_padding, groups, bias= True, dilation= 1, padding_mode = 'zeros')
   def __init__(self):
     super(ann_model,self).__init__()
-    self.conv1 = nn.Conv1d(1, 64, 21, 5, 5)
-    self.conv2 = nn.Conv1d(64, 64, 21, 1, 5)
-    self.pool1 = nn.MaxPool1d(2,2)
-
-    self.conv3 = nn.Conv1d(64, 128, 5, 1, 0)
-    self.conv4 = nn.Conv1d(128, 128, 5, 1, 0)
-    self.pool2 = nn.MaxPool1d(2,2)
-
-    self.conv5 = nn.Conv1d(128, 256, 6, 1, 0)
-    self.conv6 = nn.Conv1d(256, 256, 6, 1, 0)
-    self.pool3 = nn.AvgPool1d(3)
-    self.linear1 = nn.Linear(256,1)
-#    self.linear2 = nn.Linear(6,1)
+    self.convE1 = nn.Conv1d(1, 20, 40, 36, 18)
+    self.convE2 = nn.Conv1d(20, 40, 4, 3, 2)
+    
+    self.convD1 = nn.ConvTranspose1d(40,20,4,2)
+    self.convD2 = nn.ConvTranspose1d(20,1,40,36, 18)
+    
  
   def forward(self,x):
-    x = self.conv1(x)
+    # 1, 1, 400
+    x = self.convE1(x) # 1 -> 64, ----21, ^$+5, ...5
+    # 1, 20, 12
     x = F.relu(x)
-    x = self.conv2(x)
+    # 1, 20, 12
+    x = self.convE2(x) # 64 -> 64, ----21, ^$+5, ...1
+    # 1, 40, 5
     x = F.relu(x)
-    x = self.pool1(x)
+    encoded = x
 
-    x = self.conv3(x)
+    # 1, 40, 5
+    x = self.convD1(x) # 64 -> 64, ----21, ^$+5, ...1
+    # 1, 20, 12
     x = F.relu(x)
-    x = self.conv4(x)
-    x = F.relu(x)
-    x = self.pool2(x)
-    x = self.conv5(x)
-    x = F.relu(x)
-    x = self.conv6(x)
-    x = F.relu(x)
-    x = self.pool3(x).view(-1)
-    x = self.linear1(x)
-#    x = F.relu(x)
-#    x = self.linear2(x)
-    x = torch.sigmoid(x)
-    return x
+    # 1, 20, 12
+    x = self.convD2(x)
+    # 1, 1, 400
+
+    decoded = x
+    return encoded, decoded
+
 
 def train(model, epochs):
   criterion = nn.MSELoss()
   losslist = []
   #running_loss = 
   epochloss = 0.
-  optimizer = optim.SGD(model.parameters(),lr=0.1,weight_decay=1e-5)
-  data = loader.loader(None, 'disk', True)
+  optimizer = optim.Adam(model.parameters(),lr=0.0001,weight_decay=0.001)
+  data = loader.loader(None, 'disk')
 
   for epoch in range(epochs):
     l = 0
     print("Entering Epoch: ",epoch)
     for id, x, y in data.generator(training=True):
       optimizer.zero_grad()
-      y_hat = model(x)
-      loss = criterion(y_hat.view(1),torch.Tensor([y]))
-
+      encoded, decoded = model(x)
+      loss = criterion(decoded,x)  + 0.01*torch.abs(torch.norm(decoded)-torch.norm(x))
+      if torch.isnan(loss):
+          print('a')
       loss.backward()
 
       #if l%50==0:
@@ -73,10 +70,11 @@ def train(model, epochs):
       epochloss += loss.item()
       l += 1
     losslist.append(epochloss/l)
-    print("\n======> epoch: {}/{}, Loss:{:.6f}, l: {}".format(epoch,epochs,100*epochloss/l, l))
+    print("\n======> epoch: {}/{}, Loss:{:.4f}, l: {}".format(epoch,epochs,epochloss/l, l))
+    print("\tlast loss: {:.4f} + {:.4f}".format(criterion(decoded,x),  0.01*torch.abs(torch.norm(decoded)-torch.norm(x))))
     epochloss=0
 
-    if not (epoch % 5):
+    if 0:
       validation_loss=0.
       m = 0
       for id, x, y in data.generator(training=False):
@@ -91,7 +89,7 @@ def train(model, epochs):
 if __name__ == "__main__":
     #con = loader.connect()
     dn = ann_model()
-    train(dn, 120)
+    train(dn, 1200)
     1
 
 if 0:

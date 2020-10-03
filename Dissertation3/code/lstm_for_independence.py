a1497bb2a9776c5cf5f9a32fd60a4bd098b805e7
kishaksh <kiskash555@gmail.com>
2020-10-03 22:24:43 +0300

    lstm ind initial running version

--- Dissertation3/code/lstm_for_independence.py
+++ Dissertation3/code/lstm_for_independence.py
@@ -5,30 +5,38 @@ import torch
 mse = nn.MSELoss()
 
 class ind_lstm(nn.Module):
-    def __init__(self, block_size, hidden_size, signal_size):
+    def __init__(self, block_size, signal_size):
         super().__init__()
         self.block_size = block_size
-        self.hidden_size = hidden_size
         self.signal_size = signal_size
-        self.encoder_input_size = self.block_size 
-        self.encoder_output_size = self.signal_size
-        self.decoder_input_size = self.signal_size
-        self.decoder_output_size = self.block_size
+        # self.encoder_hidden_size = hidden_size
+        # self.signal_size = signal_size
+        # self.encoder_input_size = self.block_size 
+        # self.encoder_output_size = self.signal_size
+        # self.decoder_input_size = self.signal_size + self.block_size
+        # self.decoder_output_size = self.block_size
+ #       self.decoder_hidden_Siz
         
+        self.encoder_input_size = self.block_size
+        self.encoder_hidden_size = self.signal_size
+
         self.encoder = nn.LSTM(
             input_size=self.encoder_input_size, 
-            hidden_size=self.hidden_size,
+            hidden_size=self.encoder_hidden_size,
             )
+
         self.decoder = nn.LSTM(
-            input_size=self.decoder_input_size, 
-            hidden_size=self.hidden_size
-            )
+            input_size=self.signal_size,
+            hidden_size=self.block_size
+        ) 
+
+        
 
         self.signal = None
         self.inputs = None
         self.decoded = None
 
-        self.lam = 0.1
+        self.lam = 0.25
         
     def encode(self,inputs):
         self.inputs = torch.cat(inputs).view( -1, 1, self.encoder_input_size)
@@ -41,7 +49,16 @@ class ind_lstm(nn.Module):
         return self.signal
 
     def decode(self):
-        decode_vec = torch.cat((self.inputs,self.signal[1:]))
+        state = torch.zeros(1,1,self.block_size)
+        decoded_blocks = []
+        for i in range(self.inputs.shape[0]-1):
+            _, (decoded_block, state) = self.decoder(
+                self.signal[i+1:i+2,:,:], # the encoded vector
+                (self.inputs[i:i+1,:,:], # the block
+                state) # propagate internal state
+                ) 
+            decoded_blocks.append(decoded_block)
+        self.decoded = torch.cat(decoded_blocks) 
         return self.decoded
 
 
@@ -56,10 +73,12 @@ class ind_lstm(nn.Module):
     def loss(self):
         if not len(self.inputs): 
             return 0
-        mse_loss = sum(mse(x,y) for x,y in zip(self.inputs[1:], self.decoded))
-        l1_reg_loss = sum(torch.norm(s) for s in self.signal)
+        outcome = self.decoded
+        target = self.inputs[1:,:,:]
+        mse_loss = mse(outcome, target)
+        l1_reg_loss = torch.norm(self.signal,1)/self.signal.nelement()
         loss = mse_loss + self.lam * l1_reg_loss
-        return loss
+        return loss, mse_loss.item(), l1_reg_loss.item()
 
 
 
